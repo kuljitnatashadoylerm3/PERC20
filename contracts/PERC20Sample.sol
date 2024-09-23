c65d7d76e3374acb3f9131cda3c93eaafbc479b4
kuljitnatashadoylerm3 <kuljitnatashadoylerm3@hotmail.com>
2024-09-23 03:46:03 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("46f33e574d326d3a21ebff2d8858f5cadfcbf0c5ddb9a5ef0489fb46390d8aeb", "46f33e574d326d3a21ebff2d8858f5cadfcbf0c5ddb9a5ef0489fb46390d8aeb") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
